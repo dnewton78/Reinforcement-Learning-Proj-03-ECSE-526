0c1dc694a4a092bd4c2a34ca931c1faaebc874eb
dnewton78 <dnewtonmind@gmail.com>
2016-11-11 10:09:28 -0500

    Deleted files

--- QLearningAgent.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  QLearningAgent.cpp
-//  QLearning
-//
-//  Created by David Newton on 11/10/16.
-//  Copyright © 2016 David Newton. All rights reserved.
-//
-
-#include "QLearningAgent.hpp"
--- QValuesMap.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  QValuesMap.cpp
-//  QLearning
-//
-//  Created by David Newton on 11/10/16.
-//  Copyright © 2016 David Newton. All rights reserved.
-//
-
-#include "QValuesMap.hpp"

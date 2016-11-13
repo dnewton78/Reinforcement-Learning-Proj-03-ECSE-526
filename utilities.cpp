0e68d50b7b7a207d04334a60a9c4c84178907dd7
Kevin Ra <KRa@Kevins-MacBook-Air.local>
2016-11-12 20:38:30 -0500

    Feature extraction

--- utilities.cpp
+++ utilities.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 #include <iostream>
 #include <stdexcept>
-// #include <cstdlib>
 #include <cassert>
 #include "utilities.hpp"
 
@@ -46,9 +45,9 @@ Background::Background()
   backgroundFile.close();
 }
 
-int Background::pixelAt(int x, int y)
+int Background::pixelAt(int i, int j)
 {
-  return background[x][y];
+  return background[i][j];
 }
 
 void Background::print()
@@ -65,4 +64,83 @@ void Background::print()
     }
     cout << endl;
   }
+}
+
+
+const int Feature::TILE_WIDTH;
+const int Feature::TILE_HEIGHT;
+const int Feature::NUMOFCOLORS;
+const int Feature::BIT_SHIFT;
+const int Feature::NUMOFBASICFEATURE;
+
+Feature::Feature() {}
+
+void Feature::extractFeature(const ALEScreen& screen, vector<int>& vOut)
+{
+  assert(screen.height() == Background::HEIGHT && screen.width() == Background::WIDTH);
+
+  if (vOut.size() != 0)
+  {
+    vOut.clear();
+  }
+
+  // It's okay to integer division because we choose tile size that is divisible
+  int pixelPerTile_row = Background::HEIGHT / TILE_HEIGHT;
+  int pixelPerTile_col = Background::WIDTH / TILE_WIDTH;
+
+  int blkIdx = 0;
+  // Iterate over the tiles
+  for(int i = 0; i < TILE_HEIGHT; i++)
+  {
+    int firstPositionRow =  i * pixelPerTile_row;
+    int lastPositionRow  = (i + 1) * pixelPerTile_row;
+    for(int j = 0; j < TILE_WIDTH; j++)
+    {
+      int firstPositionCol =  j * pixelPerTile_col;
+      int lastPositionCol  = (j + 1) * pixelPerTile_col;
+
+      // Iterate the screen pixels that belong to a given tile index
+      for(int x = firstPositionRow; x < lastPositionRow; x++)
+      {
+        for(int y = firstPositionCol; y < lastPositionCol; y++)
+        {
+          // NTSC format uses 128 colors --> 7 bits
+          // According to the paper, it also support SECAM which uses 8 colors --> 3 bits
+          // We can find 3 bits in interest by shifting bits to the right by 4
+          unsigned char pixel = (screen.get(x, y) >> BIT_SHIFT);
+          unsigned char pixel_bg = (m_bg.pixelAt(x, y) >> BIT_SHIFT);
+
+          if (pixel != pixel_bg)
+          {
+            vOut.push_back(pixel + blkIdx);
+          }
+        }
+      }
+      blkIdx += NUMOFCOLORS;
+    }
+  }
+
+  // Done with basic features. Add pairwise interaction (BASS feature)
+  int curStateSize = vOut.size();
+
+  // Now form 2D matrix of pairwise comparision. Row/column have length of 
+  // basic feature size. Note that we need to convert matrix index i, j into single 
+  // index on the feature.
+  for(int i = 0; i < curStateSize; i++)
+  {
+    for(int j = i + 1; j < curStateSize; j++)
+    {
+      // vectorization of a matrix index:
+      int offset = NUMOFBASICFEATURE * vOut[i] + vOut[j];
+
+      // We only care bout comparision of tiles above the  diagonal because it we don't need
+      // 1. Comparision against itself (diagonal)
+      // 2. Same comparison but in different order (tile a vs tile b, tile b vs tile a)
+      offset = offset - ((vOut[i] + 1) * (vOut[i] + 2)) / 2;
+      // Note that we need to offset by the number of basic features so that they are not
+      // overwritten.
+      vOut.push_back(NUMOFBASICFEATURE + offset);
+    }
+  }
+
 }
--- utilities.hpp
+++ utilities.hpp
@@ -2,6 +2,7 @@
 #define UTILITIES_H
 
 #include <string>
+#include <ale_interface.hpp>
 
 using namespace std;
 
@@ -13,12 +14,28 @@ public:
   static const int HEIGHT = 210;
   
   Background();
-  int pixelAt(int x, int y);
+  int pixelAt(int i, int j);
   void print();
 
 private:
   static const string PATH_BACKGROUND;
   int background[HEIGHT][WIDTH];
+};
+
+class Feature
+{
+public:
+  Feature();
+  void extractFeature(const ALEScreen& screen, vector<int>& vOut);
+
+private:
+  static const int TILE_WIDTH = 16;
+  static const int TILE_HEIGHT = 21;
+  static const int NUMOFCOLORS = 8;
+  static const int BIT_SHIFT = 4;
+  static const int NUMOFBASICFEATURE = TILE_WIDTH * TILE_HEIGHT * NUMOFCOLORS + 1;
+
+  Background m_bg;
 };
 
 
